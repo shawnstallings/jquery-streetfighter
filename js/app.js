f00aeb81219925c71436d77feb3947974d12ad3d
Shawn Stallings <oxegeneral@yahoo.com>
2015-07-18 01:31:25 -0600

    added final text and xkey scripts

--- js/app.js
+++ js/app.js
@@ -1,29 +1,43 @@
 $(document).ready(function() {
 	$(".ryu").mouseenter(function() {
-		$(".ryu-still").hide();
+		$(".ryu-action").hide();
 		$(".ryu-ready").show();
 	})
 	.mouseleave(function() {
-		$(".ryu-ready").hide();
+		$(".ryu-action").hide();
 		$(".ryu-still").show();
 	})
 	.mousedown(function() {
 		playhadouken();
-		$(".ryu-ready").hide();
+		$(".ryu-action").hide();
 		$(".ryu-throwing").show();
 		$(".hadouken").finish().show().animate(
-			{"left": "1020px"}, 500,
+			{"left": "1020px"}, 
+			500,
 			function() {
 				$(this).hide();
-				$(this).css("left", "520px");
+				$(this).css("left", "450px");
 			}
 		);
 	})
 	.mouseup(function() {
-		$(".ryu-throwing").hide();
+		$(".ryu-action").hide();
 		$(".ryu-ready").show();
 	});
-});
+	
+	$(document).keydown(function(e) { 
+		if (e.which == 88) {
+			$(".ryu-action").hide();
+			$(".ryu-cool").show();
+		}
+	}).keyup(function(e) {
+		if(e.which == 88) {
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+		}
+	});
+	})
+
 function playhadouken () {
 		$("#hadouken-sound")[0].volume = 0.5;
 		$("#hadouken-sound")[0].load();
